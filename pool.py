c0af1b3bac0912d7c1f87e57dc73e2e8c0bcdb54
Eric Wang <39439316+ericwang2004@users.noreply.github.com>
2023-07-25 21:02:19 -0400

    Update pool.py

--- pool.py
+++ pool.py
@@ -1,43 +1,63 @@
-import pygame, sys
+import pygame, math
 
-# First thing in any pygame program - initializes pygame's internal variables.
 pygame.init()
 
-# initialize a window with the screen size you set
-size = width, height = 1000, 500
-screen = pygame.display.set_mode(size)
+width = 1000
+height = 500
 
-# create a clock, which will be used to control the program's frame rate
-clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+applegreen = (141, 182, 0)
+white = (0, 0, 0)
+
+class Cueball:
+    def __init__(self, xpos, ypos, radius, friction):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.radius = radius
+        self.friction = friction
+        self.xvel = 0
+        self.yvel = 0
+        self.xaccel = 0
+        self.yaccel = 0
+    def ismoving(self):
+        return self.xvel != 0 and self.yvel != 0
+    def shoot(self, targetx, targety):
+        if self.ismoving:
+            # we don't want to allow shooting the cue ball while it's still in motion
+            return
+        self.xvel = targetx - self.xpos
+        self.yvel = targety - self.ypos
+    def update(self):
+        pygame.draw.circle(screen, white, (self.xpos, self.ypos), self.radius)
+        if abs(self.xvel) < 0.2 and abs(self.yvel) < 0.2:
+            self.xvel = 0
+            self.yvel = 0
+            return
+        self.xpos += self.xvel
+        self.ypos += self.yvel
+        norm = math.sqrt(self.xvel * self.xvel + self.yvel * self.yvel)
+        self.xaccel = self.friction * self.xvel / norm
+        self.yaccel = self.friction * self.yvel / norm
+        self.xvel -= self.xaccel
+        self.yvel -= self.yaccel
 
 # global variables
-cueball_pos = (50, 50)
+cueball = Cueball(50, 50, 10, 0.2)
 
 running = True
 while running:
-    # tick forward at 60 frames per second
-    clock.tick(60)
-
-    # This for loop gets any keyboard, mouse, or other events that happen from user input
+    screen.fill(applegreen)
+    cueball.update()
     for event in pygame.event.get():
-        # The pygame.QUIT event happens when someone tries to close the game window.
         if event.type == pygame.QUIT:
             running = False
-        # pygame.MOUSEBUTTONDOWN occurs when the user clicks any mouse button
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Events will include what button was pushed, which you can check in if statements
             if event.button == pygame.BUTTON_LEFT:
                 print("pressed")
-        # if event.type == pygame.KEYDOWN:
-        #     # KEYDOWN happens when a keyboard key is pressed. You can check the key with event.key.
-        #     if event.key == pygame.K_SPACE:
+                x, y = pygame.mouse.get_pos()
+                cueball.shoot(x, y)
 
-    # Fill the screen with a solid color
-    screen.fill((20, 255, 20))
-
-    # Draw Cue Ball
-    pygame.draw.circle(screen, (255, 255, 255), cueball_pos, 20)
-
-    #At the end of each game loop, call pygame.display.flip() to update the screen with what you drew.
     pygame.display.update()
+
 pygame.quit()
