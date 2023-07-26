76413d3249a34f32d98a33152b9fe0f38b6cded7
Benjamin Lannis <82780306+benLannis@users.noreply.github.com>
2023-07-25 21:34:06 -0400

    Create target.py

---
+++ target.py
@@ -0,0 +1,9 @@
+import pygame, math
+
+class Target:
+    def __init__(self, xpos, ypos, outradius, inradius):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.outradius = outradius
+        self.inradius = inradius
+
