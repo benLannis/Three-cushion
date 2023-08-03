b6d58386c1e3bff2d44261c5f7d2de67dc2a16ce
Benjamin Lannis <82780306+benLannis@users.noreply.github.com>
2023-08-03 14:26:27 -0400

    Delete target.py

--- target.py
+++
@@ -1,9 +0,0 @@
-import pygame, math
-
-class Target:
-    def __init__(self, xpos, ypos, outradius, inradius):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.outradius = outradius
-        self.inradius = inradius
-
